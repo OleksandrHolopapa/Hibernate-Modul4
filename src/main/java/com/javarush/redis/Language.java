2088bdc614f1448548bd446eb010a55d82ab9ce0
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-12-21 04:31:05 +0200

    Added CityDAO class Added CountryDAO class Added CityCountry class Added Language class Changed class Country:  - add lazy initialisation to @OneToOne

---
+++ src/main/java/com/javarush/redis/Language.java
@@ -0,0 +1,15 @@
+package com.javarush.redis;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+}
